eb26016eb0ac1be8914a974c7f65e46999925b39
DostonOlimov <dostonolimov61@gmail.com>
2023-02-21 12:17:38 +0500

    21.02.2023 from doston

--- backend/controllers/control/InstructionTypeController.php
+++ backend/controllers/control/InstructionTypeController.php
@@ -70,7 +70,7 @@ class InstructionTypeController extends Controller
         $model = new InstructionType();
 
         if ($this->request->isPost) {
-            if ($model->load($this->request->post()) && $model->save()) {
+            if ($model->load($this->request->post()) && $model->save(false)) {
                 return $this->redirect(['view', 'id' => $model->id]);
             }
         } else {
